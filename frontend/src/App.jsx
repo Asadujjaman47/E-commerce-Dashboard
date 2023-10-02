8f3a8f46a3b826ace7dc57511991989fd548f77b
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-10-02 13:50:06 +0600

    Routing add in react.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
-
   return (
-    <div className='App'>
-      <h1>E-Dashboard</h1>
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Product Listing component</h1>} />
+          <Route path="/add" element={<h1>Add Product component</h1>} />
+          <Route path="/update" element={<h1>Update Product component</h1>} />
+          <Route path="/logout" element={<h1>Logout component</h1>} />
+          <Route path="/profile" element={<h1>Profile component</h1>} />
+        </Routes>
+        {/* <h1>E-Dashboard</h1> */}
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,31 @@
+// import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <div>
+      <ul className="nav-ul">
+        {/* <li>Home Page</li> */}
+        {/* <li>
+          <Link to="/">Homepage</Link>
+        </li> */}
+
+        <li>
+          <Link to="/">Products</Link>
+        </li>
+        <li>
+          <Link to="/add">Add Products</Link>
+        </li>
+        <li>
+          <Link to="/update"> Update Products</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/logout"> Logout</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
