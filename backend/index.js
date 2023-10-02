5c3e21acd8d9d134ff87cb718ff25a593a6e6492
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-10-02 14:18:57 +0600

    Node js connect with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("app is working...");
-})
+// mongodb connection
+// mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+
+
+// app.get("/", (req, res) => {
+//     res.send("app is working...");
+// })
+
+const connectDB = async() => {
+    mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+
+    // model 
+    const productSchema = new mongoose.Schema({});
+
+    // model(collection , Schema)
+    const product = mongoose.model('product', productSchema);
+    
+    // find product
+    const data = await product.find();
+
+    console.log(data);
+}
+
+connectDB();
+
 
 app.listen(5000);
