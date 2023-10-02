3dda9f3596f4314373a11ec14f61d896dbbabe61
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-10-02 16:44:12 +0600

    React js Navbar Update

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -13,43 +13,47 @@ export default function Nav() {
 
   return (
     <div>
-      <ul className="nav-ul">
-        {/* <li>Home Page</li> */}
-        {/* <li>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1200px-Node.js_logo.svg.png"
+        alt="logo"
+        className="logo"
+      />
+      {auth ? (
+        <ul className="nav-ul">
+          {/* <li>Home Page</li> */}
+          {/* <li>
           <Link to="/">Homepage</Link>
         </li> */}
 
-        <li>
-          <Link to="/">Products</Link>
-        </li>
-        <li>
-          <Link to="/add">Add Products</Link>
-        </li>
-        <li>
-          <Link to="/update"> Update Products</Link>
-        </li>
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Products</Link>
+          </li>
+          <li>
+            <Link to="/update"> Update Products</Link>
+          </li>
 
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-
-        {auth ? (
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
           <li>
             <Link onClick={logout} to="/signup">
-              Logout
+              Logout ({JSON.parse(auth).name})
             </Link>
           </li>
-        ) : (
-          <>
-            <li>
-              <Link to="/signup"> Sign Up</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </>
-        )}
-      </ul>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
+            <Link to="/signup"> Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
