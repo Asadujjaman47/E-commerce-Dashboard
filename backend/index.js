5f16aeef21b3a63fdfd63cf4ca0757e823d7bd49
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-10-02 18:09:28 +0600

    Delete Product API

--- backend/index.js
+++ backend/index.js
@@ -68,26 +68,29 @@ app.post("/login", async (req, res) => {
 });
 
 // ADD PRODUCT
-app.post("/add-product", async(req, res) => {
+app.post("/add-product", async (req, res) => {
   let product = new Product(req.body);
   let result = await product.save();
 
   res.send(result);
-})
+});
 
 // GET ALL PRODUCT
-app.get("/products", async(req, res) => {
+app.get("/products", async (req, res) => {
   let products = await Product.find();
 
-  if(products.length > 0) {
+  if (products.length > 0) {
     res.send(products);
+  } else {
+    res.send({ result: "No Products found" });
   }
-  else {
-    res.send({result: "No Products found"});
-  }
-})
+});
 
+// DELETE PRODUCT
+app.delete("/product/:id", async (req, res) => {
+  const result = await Product.deleteOne({ _id: req.params.id });
 
-
+  res.send(result);
+});
 
 app.listen(5000);
