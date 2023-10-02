823072a412eb20914ab2fd997e78a45087d341fd
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-10-02 17:19:21 +0600

    React js Form validation

--- frontend/src/components/AddProduct.jsx
+++ frontend/src/components/AddProduct.jsx
@@ -5,9 +5,15 @@ const AddProduct = () => {
   const [price, setPrice] = React.useState("");
   const [category, setCategory] = React.useState("");
   const [company, setCompnay] = React.useState("");
+  const [error, setError] = React.useState(false);
 
   const addProduct = async () => {
     // console.log(name, price, company, category);
+
+    if (!name || !price || !company || !category) {
+      setError(true);
+      return false;
+    }
 
     const userId = JSON.parse(localStorage.getItem("user"))._id;
     let result = await fetch("http://localhost:5000/add-product", {
@@ -19,7 +25,6 @@ const AddProduct = () => {
     });
     result = await result.json();
     console.log(result);
-
   };
 
   return (
@@ -34,6 +39,10 @@ const AddProduct = () => {
         onChange={(e) => setName(e.target.value)}
         placeholder="Enter product name"
       />
+      {error && !name && (
+        <span className="invalid-input">Enter valid name</span>
+      )}
+
       <input
         className="inputBox"
         type="text"
@@ -43,6 +52,10 @@ const AddProduct = () => {
         onChange={(e) => setPrice(e.target.value)}
         placeholder="Enter product price"
       />
+      {error && !price && (
+        <span className="invalid-input">Enter valid price</span>
+      )}
+
       <input
         className="inputBox"
         type="text"
@@ -52,6 +65,10 @@ const AddProduct = () => {
         onChange={(e) => setCategory(e.target.value)}
         placeholder="Enter product category"
       />
+      {error && !category && (
+        <span className="invalid-input">Enter valid category</span>
+      )}
+
       <input
         className="inputBox"
         type="text"
@@ -61,6 +78,10 @@ const AddProduct = () => {
         onChange={(e) => setCompnay(e.target.value)}
         placeholder="Enter product company"
       />
+      {error && !company && (
+        <span className="invalid-input">Enter valid company</span>
+      )}
+      
       <button onClick={addProduct} className="appButton">
         Add Product
       </button>
